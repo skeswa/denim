8e3a9f77cc14498b39d7d7ddc14991af683e8150
Sandile Keswa <me@sandile.io>
2023-02-19 18:21:08 -0500

    I'm starting over.

--- lang/text/loc.go
+++
@@ -1,7 +0,0 @@
-package text
-
-// Points to a spot in a file or piece of text.
-type Loc struct {
-	// This is the 0-based index of this location from the start of the file, in bytes
-	Start int32
-}
--- lang/text/span.go
+++
@@ -1,9 +0,0 @@
-package text
-
-// Reference to some stretch of text.
-type Span struct {
-	// String that contains this `Span`.
-	Text string
-	// Information about where this `Span` lives within `Text`.
-	Range Range
-}
