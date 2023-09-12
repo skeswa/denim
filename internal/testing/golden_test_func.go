42a232b73247f769085c71eecd20f690efe662a1
Sandile Keswa <me@sandile.io>
2023-09-12 15:48:19 -0700

    Starting over. Clearing house.

--- internal/testing/golden_test_func.go
+++
@@ -1,5 +0,0 @@
-package denimtesting
-
-// Function used by golden tests that accepts test [input] and returns golden
-// test output, or an [error] if something goes wrong.
-type GoldenTestFunc = func(input string) (string, error)
