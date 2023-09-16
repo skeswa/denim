5b94be81c34adb73240f6ed1f8da3e8a6e862540
Sandile Keswa <me@sandile.io>
2023-09-16 01:05:02 -0400

    Moved tokenize tests into its file

--- crates/tokenizer/src/lib.rs
+++ crates/tokenizer/src/lib.rs
@@ -10,18 +10,3 @@ mod testing;
 mod token;
 mod token_kind;
 pub mod tokenize;
-
-#[cfg(test)]
-mod tests {
-    use expect_test::expect_file;
-
-    use crate::testing::GoldenTestCases;
-
-    #[test]
-    fn ok_goldens() {
-        for golden_test_case in GoldenTestCases::in_dir("ok").into_iter() {
-            expect_file![golden_test_case.tokens_path.clone()]
-                .assert_eq(&golden_test_case.stringify_tokenized_source());
-        }
-    }
-}
